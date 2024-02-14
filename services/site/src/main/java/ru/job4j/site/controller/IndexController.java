c5b93de9fd778add1d73e666cb0d0bac9139285b
Admin <Serger9393@mail.ru>
2024-02-14 04:29:59 +0300

    4. Микросервисные системы [#338434]

--- services/site/src/main/java/ru/job4j/site/controller/IndexController.java
+++ services/site/src/main/java/ru/job4j/site/controller/IndexController.java
@@ -3,15 +3,19 @@ package ru.job4j.site.controller;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import ru.job4j.site.service.AuthService;
-import ru.job4j.site.service.CategoriesService;
-import ru.job4j.site.service.InterviewsService;
-import ru.job4j.site.service.NotificationService;
+import org.springframework.web.bind.annotation.RequestParam;
+import ru.job4j.site.dto.InterviewDTO;
+import ru.job4j.site.dto.ProfileDTO;
+import ru.job4j.site.service.*;
 
 import javax.servlet.http.HttpServletRequest;
+
+import java.util.*;
+import java.util.stream.Collectors;
 
 import static ru.job4j.site.controller.RequestResponseTools.getToken;
 
@@ -23,6 +27,7 @@ public class IndexController {
     private final InterviewsService interviewsService;
     private final AuthService authService;
     private final NotificationService notifications;
+    private final ProfilesService profilesService;
 
     @GetMapping({"/", "index"})
     public String getIndexPage(Model model, HttpServletRequest req) throws JsonProcessingException {
@@ -41,7 +46,14 @@ public class IndexController {
         } catch (Exception e) {
             log.error("Remote application not responding. Error: {}. {}, ", e.getCause(), e.getMessage());
         }
-        model.addAttribute("new_interviews", interviewsService.getByType(1));
+        Page<InterviewDTO> interviewDTOList = interviewsService.getAll(getToken(req), 0, 20);
+        Set<ProfileDTO> userList = interviewDTOList.stream()
+                .map(x -> profilesService.getProfileById(x.getSubmitterId()))
+                .filter(Optional::isPresent)
+                .map(Optional::get)
+                .collect(Collectors.toSet());
+        model.addAttribute("new_interviews", interviewDTOList);
+        model.addAttribute("users", userList);
         return "index";
     }
 }
